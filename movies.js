525d32b7002e13c6fa85b3772abd49f0fafca566
Cindy Hech <archerychinadiva@gmail.com>
2026-02-13 01:35:48 -0500

    JavaScript API successfully and dynamically integrated

--- movies.js
+++ movies.js
@@ -5,7 +5,8 @@ const movieListEl = document.querySelector('.movies');
 async function renderMovies() {
  const movies = await fetch ("http://www.omdbapi.com/?apikey=61ba8310&s=game");
  const moviesData = await movies.json();
- movieListEl.innerHTML = moviesData.map((movie) => moviesHTML(movie)).join("");
+ console.log(moviesData)
+ movieListEl.innerHTML = moviesData.Search.map((movie) => moviesHTML(movie)).join("");
 }
 
 
@@ -14,10 +15,10 @@ async function renderMovies() {
 function moviesHTML(movie) {
     return `<div class="movie">
         <figure class="movie__img--wrapper">
-        <img class="movie__img" src="https://m.media-amazon.com/images/M/MV5BMTNhMDJmNmYtNDQ5OS00ODdlLWE0ZDAtZTgyYTIwNDY3OTU3XkEyXkFqcGc@._V1_SX300.jpg" alt="">
+        <img class="movie__img" src="${movie.Poster}" alt="">
         </figure>
-        <h2 class="movie__title">Game of Thrones</h2>
-        <h4 class="movie__year">2011–2019</h4>
+        <h2 class="movie__title">${movie.Title}</h2>
+        <h4 class="movie__year">${movie.Year}</h4>
         <button class="movie__button">Learn More</button>
     </div>`;
 }
