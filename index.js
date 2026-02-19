de3664d8d9210a22762d63653d819490372f9639
Cindy Hech <archerychinadiva@gmail.com>
2026-02-18 19:13:45 -0500

    Fixing localhost url

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+const rerouteBtn = document.querySelector(".reroute-btn")
+
+
 function openMenu() {
  document.body.classList += " menu--open"
 }
 
 function closeMenu() {
  document.body.classList.remove('menu--open')
+}
+
+function rerouteToMovies() {
+ window.location.href = `${window.location.origin}/M4-Final-Project/movies.html`
 }
